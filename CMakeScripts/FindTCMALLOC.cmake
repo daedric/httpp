8bc9f7a6f0d9a878978fc525eea98ab16dd9bac9
Thomas Sanchez <thomas.sanchz@gmail.com>
2014-01-09 12:26:42 +0100

    Add tcmalloc optional dependency

---
+++ CMakeScripts/FindTCMALLOC.cmake
@@ -0,0 +1,22 @@
+# Part of HTTPP.
+#
+# Distributed under the 3-clause BSD licence (See LICENCE.TXT file at the
+# project root).
+#
+# Copyright (c) 2014 Thomas Sanchez.  All rights reserved.
+#
+
+FIND_LIBRARY(TCMALLOC_LIBRARIES
+  NAMES
+    tcmalloc
+    tcmalloc_minimal
+
+  PATHS
+    /usr/lib
+    /usr/local/lib
+    /opt/local/lib
+)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(TCMALLOC DEFAULT_MSG TCMALLOC_LIBRARIES)
+MARK_AS_ADVANCED(TCMALLOC_LIBRARIES)
