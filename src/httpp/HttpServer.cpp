8218c56bc7a858fb2da606f64f4929ae0e282bfd
Thomas Sanchez <thomas.sanchz@gmail.com>
2015-06-30 15:21:46 +0200

    Disable sslv3 and tlsv1 in HTTPP's https code

--- src/httpp/HttpServer.cpp
+++ src/httpp/HttpServer.cpp
@@ -37,6 +37,8 @@ struct HttpServer::Acceptor : boost::asio::ip::tcp::acceptor
 
         auto flags = boost::asio::ssl::context::default_workarounds |
                      boost::asio::ssl::context::no_sslv2 |
+                     boost::asio::ssl::context::no_sslv3 |
+                     boost::asio::ssl::context::no_tlsv1 |
                      boost::asio::ssl::context::single_dh_use;
 
         ssl_ctx->set_options(flags);
