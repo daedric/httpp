30b9e9a70d25f0c45c6317ade6eec61e62e4a797
Thomas Sanchez <thomas.sanchz@gmail.com>
2015-10-27 14:31:43 +0100

    Add convenient operator overload for the LazyDecodedValue. This fixes the tests

--- include/httpp/utils/LazyDecodedValue.hpp
+++ include/httpp/utils/LazyDecodedValue.hpp
@@ -13,6 +13,7 @@
 # define HTTPP_UTILS_LAZYDECODEDVALUE_HPP
 
 # include <string>
+# include <iosfwd>
 # include <boost/utility/string_ref.hpp>
 
 namespace HTTPP
@@ -24,6 +25,8 @@ namespace UTILS
 class LazyDecodedValue
 {
 public:
+    LazyDecodedValue() = default;
+    LazyDecodedValue(const char* str, size_t len = 0);
     LazyDecodedValue(boost::string_ref str);
     ~LazyDecodedValue() = default;
 
@@ -32,15 +35,30 @@ public:
     LazyDecodedValue& operator=(const LazyDecodedValue&) = default;
     LazyDecodedValue& operator=(LazyDecodedValue&&) = default;
 
+    bool operator==(const LazyDecodedValue& rhs) const noexcept;
+
     operator const std::string&() const;
     const std::string& string() const;
+
+    const boost::string_ref& ref() const noexcept;
 
 private:
     boost::string_ref raw_value_;
     mutable std::string decoded_value_;
 };
 
+
+bool operator==(const LazyDecodedValue&, const char*);
+bool operator==(const char*, const LazyDecodedValue&);
+
+bool operator==(const LazyDecodedValue&, const std::string&);
+bool operator==(const std::string&, const LazyDecodedValue&);
+
+bool operator==(const LazyDecodedValue&, const boost::string_ref&);
+bool operator==(const boost::string_ref&, const LazyDecodedValue&);
+
 std::string to_string(const LazyDecodedValue&);
+std::ostream& operator<<(std::ostream&, const LazyDecodedValue&);
 
 } // namespace UTILS
 } // namespace HTTPP
--- src/httpp/utils/LazyDecodedValue.cpp
+++ src/httpp/utils/LazyDecodedValue.cpp
@@ -1,5 +1,8 @@
 #include "httpp/utils/LazyDecodedValue.hpp"
 
+#include <ostream>
+#include <cstring>
+
 #include "httpp/utils/URL.hpp"
 
 namespace HTTPP
@@ -10,6 +13,16 @@ namespace UTILS
 LazyDecodedValue::LazyDecodedValue(boost::string_ref str)
 : raw_value_(std::move(str))
 {
+}
+
+LazyDecodedValue::LazyDecodedValue(const char* str, size_t len)
+: raw_value_(str, len ? len : ::strlen(str))
+{
+}
+
+bool LazyDecodedValue::operator==(const LazyDecodedValue& rhs) const noexcept
+{
+    return raw_value_ == rhs.raw_value_;
 }
 
 LazyDecodedValue::operator const std::string&() const
@@ -27,9 +40,49 @@ const std::string& LazyDecodedValue::string() const
     return decoded_value_;
 }
 
+const boost::string_ref& LazyDecodedValue::ref() const noexcept
+{
+    return raw_value_;
+}
+
 std::string to_string(const LazyDecodedValue& val)
 {
     return val.string();
+}
+
+bool operator==(const LazyDecodedValue& lhs, const char* rhs)
+{
+    return lhs.string() == rhs;
+}
+
+bool operator==(const char* lhs, const LazyDecodedValue& rhs)
+{
+    return lhs == rhs.string();
+}
+
+bool operator==(const LazyDecodedValue& lhs, const std::string& rhs)
+{
+    return lhs.string() == rhs;
+}
+
+bool operator==(const std::string& lhs, const LazyDecodedValue& rhs)
+{
+    return lhs == rhs.string();
+}
+
+bool operator==(const LazyDecodedValue& lhs, const boost::string_ref& rhs)
+{
+    return lhs.string() == rhs;
+}
+
+bool operator==(const boost::string_ref& lhs, const LazyDecodedValue& rhs)
+{
+    return lhs == rhs.string();
+}
+
+std::ostream& operator<<(std::ostream& os, const LazyDecodedValue& v)
+{
+    return os << v.string();
 }
 
 } // namespace UTILS
